20988428f69a508cc0d32768da72f3d13c3f8b92
Carlos Valiente <carlos@pepelabs.net>
2016-01-26 10:27:37 +0000

    tox support

---
+++ README.rst
@@ -0,0 +1,8 @@
+container-cluster - A tool to manage container clusters
+=======================================================
+
+``container-cluster`` is a command-line tool to manage small `Kubernetes`_
+clusters for learning and development purposes.
+
+
+.. _Kubernetes: http://kubernetes.io/
---
+++ setup.py
@@ -0,0 +1,60 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+def read_requirements():
+    ret = []
+    fname = os.path.join(os.path.dirname(__file__), "requirements.txt")
+    with open(fname, "r") as f:
+        for line in f:
+            line = line.strip()
+            if line and not line.startswith("#"):
+                ret.append(line)
+    return ret
+
+
+def read_long_description():
+    with open("README.rst", "r") as f:
+        return f.read()
+
+
+setup(
+    name="container-cluster",
+    version="0.1.0",
+    description="Tools to manage container clusters",
+    long_description=read_long_description(),
+    url="https://github.com/carletes/container-cluster",
+    author="Carlos Valiente",
+    author_email="carlos@pepelabs.net",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+
+    package_dir={
+        "containercluster": "containercluster",
+    },
+    packages=find_packages(),
+    package_data={
+        "containercluster": [
+            "etcd-cloud-config.yaml",
+            "worker-cloud-config.yaml",
+        ]
+    },
+    entry_points={
+        "console_scripts": [
+            "container-cluster = containercluster.cmdline:main",
+        ]
+    },
+    install_requires=read_requirements(),
+
+    zip_safe=False,
+)
